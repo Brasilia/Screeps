ac712a606aa9df0ac10593bedb21dbf256391831
Brasilia <ksuspec@gmail.com>
2016-11-23 04:22:07 -0200

    Distribute Container Energy

--- prototype.creep.js
+++ prototype.creep.js
@@ -17,25 +17,56 @@ module.exports = function() {
     };
     Creep.prototype.withdrawEnergyFromClosestContainer =
     function(){
-        this.findClosestByPath(this.room.memory.containers).memory.demand += this.carryCapacity;
-
-        if (!this.memory.closestContainer){
-            this.memory.closestContainer = this.findClosestByPath(this.room.memory.containersNotEmpty);
-            if (!this.memory.closestContainer){
+        //var closestContID = this.pos.findClosestByPath(this.room.memory.containers).id;
+        if (!this.memory.closestContainerID){
+            this.memory.closestContainerID = this.pos.findClosestByPath(this.room.memory.containersNotEmpty).id;
+            if (!this.memory.closestContainerID){
                 //try alternative energy source - otimizar
                 this.harvestFromSource();
             }
         } else { //already knows destination
-            var withdrew = this.withdraw(this.memory.closestContainer, RESOURCE_ENERGY);
+            var targetContainer = Game.getObjectById(this.memory.closestContainerID);
+            var withdrew = this.withdraw(targetContainer, RESOURCE_ENERGY);
             if (withdrew == ERR_NOT_IN_RANGE){
-                this.moveTo(this.memory.closestContainer);
+                this.moveTo(targetContainer);
             } else if(withdrew == OK || withdrew == ERR_NOT_ENOUGH_RESOURCES){
-                this.memory.closestContainer = null;
+                this.memory.closestContainerID = null;
                 if (withdrew == OK){
                     return OK;
                 }
             }
-
+        }
+    };
+    Creep.prototype.distributeContainerEnergy =
+    function() {
+        if (this.carry.energy == 0){
+            this.memory.loaded = false;
+        }
+        if (this.carry.energy == this.carryCapacity){
+            this.memory.loaded = true;
+        }
+        if (!this.memory.decided){
+            var destination = this.room.controller.pos.findClosestByRange(this.room.memory.containers);
+            var otherContainers = _.filter(this.room.memory.containersNotEmpty, (cont) => cont.id != destination.id && cont.store[RESOURCE_ENERGY] > cont.storeCapacity/2);
+            var origin = otherContainers[0];
+            if (destination && origin ){
+                this.memory.decided = true;
+            }
+        }
+        if (this.memory.decided){
+            if(!this.memory.loaded){ // not loaded - go load
+                if(this.withdraw(origin, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+                    this.moveTo(origin);
+                } else if (this.withdraw(origin, RESOURCE_ENERGY) == ERR_NOT_ENOUGH_RESOURCES){
+                    this.memory.decided = false;
+                }
+            } else { //loaded - go transfer
+                if (this.transfer(destination, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+                    this.moveTo(destination);
+                } else {
+                    this.memory.decided = false;
+                }
+            }
         }
     };
 };
